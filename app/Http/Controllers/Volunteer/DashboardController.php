0173c06766a7c237c44ea47b54854d1fc67c31bf
Adam <adam@Adams-MacBook-Pro.local>
2026-04-12 19:28:50 +0100

    better apply error

--- app/Http/Controllers/Volunteer/DashboardController.php
+++ app/Http/Controllers/Volunteer/DashboardController.php
@@ -17,7 +17,10 @@ class DashboardController extends Controller
             ->orderByDesc('created_at')
             ->get();
 
+        $now = now();
         $openEvents = Event::where('status', 'open')
+            ->where(fn($q) => $q->whereNull('applications_open_at')->orWhere('applications_open_at', '<=', $now))
+            ->where(fn($q) => $q->whereNull('applications_close_at')->orWhere('applications_close_at', '>=', $now))
             ->whereDoesntHave('applications', fn($q) => $q->where('user_id', $user->id))
             ->orderBy('starts_at')
             ->get();
